14d984fcd5ff5975204f9e7d37f83079ce8e1e1d
Ann (Primpraow Mulling) <105256768+AnnMulling@users.noreply.github.com>
2023-10-10 16:48:26 -0500

    navlink and link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+
+import './index.css';
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <nav className='comp nav' >
+        <ul>
+          <li><NavLink activeStyle={{ fontWeight: 'bold'}} activeClassName='purple' exact to='/'>Home</NavLink></li>
+          <li><NavLink activeStyle={{ fontWeight: 'bold'}} activeClassName='purple' exact to='/movies'>Movies</NavLink></li>
+          <li><NavLink activeStyle={{ fontWeight: 'bold'}} activeClassName='purple' exact to='/stocks'>Stocks</NavLink></li>
+        </ul>
+      </nav>
+     
       <Switch>
         <Route exact path='/'>
           <Home />
